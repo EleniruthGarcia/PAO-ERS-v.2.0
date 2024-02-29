a7197e9dbd739b53651ac133d4e2462711d4d1f5
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-02-29 11:19:00 +0800

    Update colors in app.pcss and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        diligence: 'rgb(var(--color-diligence) / <alpha-value>)',
-        verdict: 'rgb(var(--color-verdict) / <alpha-value>)',
-        equity: 'rgb(var(--color-equity) / <alpha-value>)',
-        trust: 'rgb(var(--color-trust) / <alpha-value>)',
-        innocence: 'rgb(var(--color-innocence) / <alpha-value>)',
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				diligence: 'rgb(var(--color-diligence) / <alpha-value>)',
+				verdict: 'rgb(var(--color-verdict) / <alpha-value>)',
+				equity: 'rgb(var(--color-equity) / <alpha-value>)',
+				trust: 'rgb(var(--color-trust) / <alpha-value>)',
+				innocence: 'rgb(var(--color-innocence) / <alpha-value>)',
+				oath: 'rgb(var(--color-oath) / <alpha-value>)',
+			},
+		},
+	},
+	plugins: [],
 };
