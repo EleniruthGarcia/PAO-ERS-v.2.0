ac590f9795d6bd55db82b441ab34de03856eccd7
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-02 13:25:18 +0800

    Fix parent function call in lawyers page server load

--- src/routes/lawyers/+page.server.ts
+++ src/routes/lawyers/+page.server.ts
@@ -2,7 +2,9 @@ import prisma from '$lib/server/prisma'
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ parent, locals }) => {
+    await parent()
+
     if (locals.user?.role !== 'admin')
         redirect(302, '/')
 
