6620d65bde161a406c5f2e258e6a323e1ef7d787
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-15 00:22:58 +0800

    Add template for generating reports

--- src/lib/utils/forms/allReport.ts
+++ src/lib/utils/forms/allReport.ts
@@ -1,12 +1,14 @@
 import ExcelJS from 'exceljs';
 import type { Client } from '@prisma/client';
 
+import template from '$lib/assets/reports.xlsx?raw';
+
 import * as F10 from './F.10';
 import * as F17 from './F.017';
 
 export const generateReports = async (clients: Client[]) => {
 	const workbook = new ExcelJS.Workbook();
-	await workbook.xlsx.readFile('src/lib/utils/forms/reports.xlsx');
+	await workbook.xlsx.readFile(template);
 
 	await F10.addRow(workbook.getWorksheet('F.10'), clients);
 	await F17.addRow(workbook.getWorksheet('F.17'), clients);
