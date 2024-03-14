3f19ff4b20d926ad2acaee52dbe0de84ab961011
ReduxX5 <rance.bobadilla5@gmail.com>
2024-03-14 23:16:00 +0800

    updated forms

--- src/lib/utils/forms/F.10.ts
+++ src/lib/utils/forms/F.10.ts
@@ -3,19 +3,19 @@ import type { Client } from '@prisma/client';
 import ExcelJS from 'exceljs';
 
 // F.10 - BRGY. Service Day Outreach Program
+
 export const addRow = async (worksheet: ExcelJS.Worksheet | undefined, clients: Client[]) => {
 	if (!worksheet) return;
 
 	for (let i = 0; i < clients.length; i++) {
 		const client = clients[i];
 		worksheet.insertRow(14, [
-			`${client.id}`,
+			client.createdAt,
 			`${client.firstName} ${client.middleName ? client.middleName : ''} ${client.lastName} ${client.nameSuffix ? client.nameSuffix : ''}`,
-			client.address,
-			client.age,
 			client.sex,
-			client.contactNumber ? client.contactNumber : '',
-			client.email ? client.email : '',
+			'',
+			'',
+			''
 		], 'o');
 	}
 };
