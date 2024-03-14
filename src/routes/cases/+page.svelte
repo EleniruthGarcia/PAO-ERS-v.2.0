b69df44dfa7a231aef50e99909721d1e67241cb8
Marilag <offcllance@gmail.com>
2024-03-14 21:45:53 +0800

    Update Client UI

--- src/routes/cases/+page.svelte
+++ src/routes/cases/+page.svelte
@@ -1,109 +1,101 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
+	import SvgIcon from '@jamescoyle/svelte-icon';
+	import { mdiPencil } from '@mdi/js';
+	import { mdiTrashCan } from '@mdi/js';
 	import Loading from '$lib/components/Loading.svelte';
+	import Cases from '$lib/icons/Cases.svelte';
 
 	export let data: PageServerData;
+
+	let time = new Date();
+
+	setInterval(() => (time = new Date()));
 </script>
 
-<main class="h-screen w-full flex flex-col p-12 gap-6 bg-witness">
-	<span>
-		<h1 class="text-3xl font-bold">Cases</h1>
-		<p>See all cases here.</p>
-	</span>
+<main
+	class="h-screen w-screen py-12 p-6 lg:p-12 lg:pl-14 flex flex-col gap-4 bg-witness text-diligence lg:overflow-y-hidden"
+>
+	<div class="hidden md:block fixed top-10 right-10 text-right">
+		<p class="font-bold text-diligence text-sm">
+			{Intl.DateTimeFormat('en-PH', {
+				weekday: 'long',
+				year: 'numeric',
+				month: 'long',
+				day: 'numeric',
+				hour: 'numeric',
+				minute: 'numeric',
+				second: 'numeric'
+			}).format(time)}
+		</p>
+		<p class="text-equity text-sm">
+			{Intl.DateTimeFormat('en-PH', { timeZoneName: 'long' })
+				.formatToParts(time)
+				.find((part) => part.type === 'timeZoneName')?.value}
+		</p>
+	</div>
 
-	<div class="flex flex-col md:grid md:grid-cols-2 gap-4">
-		<div class="flex flex-col gap-4 bg-oath border border-innocence rounded-md p-4">
-			<h1 class="text-xl font-bold">Clients</h1>
-			<div class="flex flex-col gap-4">
-				{#await data.clients}
-					<div><Loading /></div>
-				{:then clients}
-					{#if clients.length === 0}
-						<p>No clients found!</p>
-					{:else}
-						<ul>
-							{#each clients as client}
-								<li>
-									<a href="/clients/{client.id}"
-										>{client.firstName +
-											' ' +
-											client.middleName +
-											' ' +
-											client.lastName +
-											(client.nameSuffix ? ' ' + client.nameSuffix : '')}</a
-									>
-								</li>
-							{/each}
-						</ul>
-					{/if}
-				{:catch error}
-					<p>{error.message}</p>
-				{/await}
-			</div>
+	
+	<div class="pl-6 lg:pl-0 mb-4">
+		<h2 class="font-bold mb-2">Cases</h2>
+		<span class="font-bold">All cases are listed here.</span> Click on a case to view more information.
+	</div>
 
-			<span>
-				<a href="/clients/add" class="px-4 py-2 rounded-lg bg-trust text-diligence">New Client</a>
-			</span>
-		</div>
-		<div class="flex flex-col gap-4 bg-oath border border-innocence rounded-md p-4">
-			<h1 class="text-xl font-bold">Requests</h1>
-			<div class="flex flex-col gap-4">
-				{#await data.requests}
-					<div class="animate-pulse flex flex-col gap-1">
-						<div class="rounded-full bg-slate-500 h-2 w-40"></div>
-						<div class="rounded-full bg-slate-500 h-1 w-20"></div>
-						<div class="rounded-full bg-slate-500 h-1 w-10"></div>
-					</div>
-				{:then requests}
-					{#if requests.length === 0}
-						<p>No requests found!</p>
-					{:else}
-						<ul>
-							{#each requests as request}
-								<li>
-									<a href="/requests/{request.id}">{request.id}</a>
-								</li>
-							{/each}
-						</ul>
-					{/if}
-				{:catch error}
-					<p>{error.message}</p>
-				{/await}
-			</div>
-
-			<span>
-				<a href="/requests/add" class="px-4 py-2 rounded-lg bg-trust text-diligence">New Request</a>
-			</span>
-		</div>
-		<div class="md:col-span-2 flex flex-col gap-4 bg-oath border border-innocence rounded-md p-4">
-			<h1 class="text-xl font-bold">Cases</h1>
-			<div class="flex flex-col gap-4">
-				{#await data.cases}
-					<div class="animate-pulse flex flex-col gap-1">
-						<div class="rounded-full bg-slate-500 h-2 w-40"></div>
-						<div class="rounded-full bg-slate-500 h-1 w-20"></div>
-						<div class="rounded-full bg-slate-500 h-1 w-10"></div>
-					</div>
-				{:then cases}
+	<div class="flex flex-col gap-4">
+		{#await data.cases}
+			<div><Loading /></div>
+		{:then cases}
+			<div class="flex flex-col gap-4 lg:col-span-7 col-span-10 p-2 pt-0">
+				<div
+					class="flex flex-col gap-4 py-6 rounded-lg border border-innocence bg-witness shadow-md"
+				>
+					<h4 class="font-bold text-equity px-6">Cases</h4>
 					{#if cases.length === 0}
-						<p>No cases found!</p>
+						<p>No cases found.</p>
 					{:else}
-						<ul>
-							{#each cases as _case}
-								<li>
-									<a href="/cases/{_case.id}">{_case.id}</a>
-								</li>
-							{/each}
-						</ul>
+						<table class="text-left w-full">
+							<thead class="w-full">
+								<tr class=" px-6 flex w-full border border-0 border-t border-innocence">
+									<th class="p-3 w-1/3">Title</th>
+									<th class="p-3 w-1/4 text-center">Status</th>
+									<th class="p-3 w-1/4 text-center">Last Action Taken</th>
+									<th class="p-3 w-1/6"></th>
+								</tr>
+							</thead>
+							<tbody class="text-sm flex flex-col overflow-y-scroll w-full h-72">
+								{#each cases as _case}
+									<a href="/cases/{_case.id}">
+										<tr
+											class="h-12 px-6 flex w-full hover:bg-oath border border-0 border-b border-t border-innocence"
+										>
+											<td class="font-bold p-3 w-1/3">{_case.id}</td>
+											<td class="p-3 w-1/4 text-center">{_case.status}</td>
+											<td class="p-3 w-1/4 text-center">{_case.lastActionTaken}</td>
+											<td class="w-1/6 flex items-center justify-end gap-2">
+												<a href="/cases/{_case?.id}/edit"
+													><button class="flex items-center gap-2 px-2 lg:px-4"
+														><SvgIcon size="15px" type="mdi" path={mdiPencil}></SvgIcon><span
+															class="hidden lg:block">Edit</span
+														></button
+													></a
+												>
+												<a href="/cases/{_case?.id}/delete"
+													><button class="px-2 bg-diligence text-oath"
+														><SvgIcon size="20px" type="mdi" path={mdiTrashCan}></SvgIcon></button
+													></a
+												>
+											</td>
+										</tr>
+									</a>
+								{/each}
+							</tbody>
+						</table>
 					{/if}
-				{:catch error}
-					<p>{error.message}</p>
-				{/await}
+					<div class="flex gap-4 px-6">
+						<a href="/cases/add"><button class="bg-trust" type="submit">New Case</button></a>
+					</div>
+				</div>
 			</div>
-
-			<span>
-				<a href="/cases/add" class="px-4 py-2 rounded-lg bg-trust text-diligence">New Case</a>
-			</span>
-		</div>
+		{/await}
 	</div>
 </main>
