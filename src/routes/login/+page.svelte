c3a4483e83a512230f0144e1b94707acc97f59b3
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-28 00:04:08 +0800

    Update LoginForm import and add form validation

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types.js';
-	import LoginForm from './login-form.svelte';
+	import LoginForm from './LoginForm.svelte';
 	export let data: PageData;
 </script>
 
