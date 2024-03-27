b0de595ecd8b8881672226832904366a476628e8
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-03-28 02:59:07 +0800

    Add layout and page components for home route

---
+++ src/routes/(home)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import NightToggle from '$lib/components/night-toggle.svelte';
+</script>
+
+<header
+	class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+>
+	<div class="container flex h-14 max-w-screen-2xl items-center">
+		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+			<NightToggle />
+		</div>
+	</div>
+</header>
+
+<slot />
