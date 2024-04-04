470b38b21ac59828f6497a5c35be0b65cc976d37
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-04-04 17:02:32 +0800

    Update package.json and svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import auto_adapter from '@sveltejs/adapter-auto';
+import vercel_adapter from '@sveltejs/adapter-vercel';
 import node_adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -12,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.VERCEL ? auto_adapter() : node_adapter()
+		adapter: process.env.VERCEL ? vercel_adapter() : node_adapter()
 	}
 };
 
