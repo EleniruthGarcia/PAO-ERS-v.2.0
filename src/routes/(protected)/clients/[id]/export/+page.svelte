29583eee4c4e2c311a1c465949f26b66c6360ccf
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-04-07 01:01:44 +0800

    Add error handling for empty interview sheet data

--- src/routes/(protected)/clients/[id]/export/+page.svelte
+++ src/routes/(protected)/clients/[id]/export/+page.svelte
@@ -4,11 +4,16 @@
 	import type { PageServerData } from './$types';
 
 	import { Button } from '$lib/components/ui/button';
+	import { toast } from 'svelte-sonner';
 
 	export let data: PageServerData;
 	export let form;
 
-	$: form?.interview_sheet && save(form.interview_sheet);
+	$: form?.interview_sheet &&
+		form?.interview_sheet?.error === undefined &&
+		save(form.interview_sheet);
+
+	$: form?.interview_sheet?.error && toast.error('No interview sheet data found!');
 </script>
 
 <form method="POST" use:enhance>
