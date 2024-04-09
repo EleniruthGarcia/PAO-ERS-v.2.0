cc89e73613376d3a89d6b02ce28e73c1679d69b4
Marilag <offcllance@gmail.com>
2024-04-09 15:35:29 +0800

    Apply theme

--- src/lib/components/SidebarLink.svelte
+++ src/lib/components/SidebarLink.svelte
@@ -6,8 +6,8 @@
 
 	$: navLink = (href: string) =>
 		cn(
-			'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary',
-			$page.url.pathname.startsWith(href) && 'bg-muted text-primary'
+			'flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-foreground',
+			$page.url.pathname.startsWith(href) && 'bg-secondary text-foreground'
 		);
 
 	export let link: { href: string; label: string; icon: any };
