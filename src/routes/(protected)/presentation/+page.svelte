0978bf39514fca38bb7fdeabe9274e735bb8a44a
Marilag <offcllance@gmail.com>
2024-04-11 21:14:33 +0800

    Update Presentation

--- src/routes/(protected)/presentation/+page.svelte
+++ src/routes/(protected)/presentation/+page.svelte
@@ -13,6 +13,7 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import NightToggle from '$lib/components/NightToggle.svelte';
 	import { Progress } from '$lib/components/ui/progress';
+	import { Separator } from '$lib/components/ui/separator';
 	import * as Tabs from '$lib/components/ui/tabs';
 
 	import * as Avatar from '$lib/components/ui/avatar';
@@ -59,7 +60,7 @@
 			</Card.Header>
 			<Card.Content class="grid auto-rows-max items-start justify-center gap-3 pb-12">
 				<div class="grid grid-cols-4 gap-3 pb-4">
-					<div class="border-1 flex rounded-md border gap-4 p-4 items-center">
+					<div class="border-1 flex items-center gap-4 rounded-md border p-4">
 						<Avatar.Root>
 							<Avatar.Image
 								src="https://scontent.cdninstagram.com/v/t51.2885-19/434695257_1380886895897660_7480150391130763231_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=jqgQUiXGtNkAb4QpKyl&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfDwFiGrOPg_EEoIMdunzVUw6wH0wOS0aYyIpWscQfa1QQ&oe=661DBDA9&_nc_sid=10d13b"
@@ -72,11 +73,11 @@
 							<span class="text-sm text-muted-foreground">Project Manager</span>
 						</div>
 					</div>
-					<div class="border-1 flex rounded-md border gap-4 p-4 items-center">
+					<div class="border-1 flex items-center gap-4 rounded-md border p-4">
 						<Avatar.Root>
 							<Avatar.Image
 								src="https://scontent.cdninstagram.com/v/t51.2885-19/416080657_191854717349011_6773820498928470338_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=vGYY-cDOBWMAb5zmDw6&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfDaO1nok4uKz8hsuniriiNtNftBZ2eurV_RkYVgURd6AA&oe=661DB194&_nc_sid=10d13b"
-                                alt="@shadcn"
+								alt="@shadcn"
 							/>
 							<Avatar.Fallback>DB</Avatar.Fallback>
 						</Avatar.Root>
@@ -85,11 +86,11 @@
 							<span class="text-sm text-muted-foreground">Back-end Developer</span>
 						</div>
 					</div>
-					<div class="border-1 flex rounded-md border gap-4 p-4 items-center">
+					<div class="border-1 flex items-center gap-4 rounded-md border p-4">
 						<Avatar.Root>
 							<Avatar.Image
 								src="https://scontent.cdninstagram.com/v/t51.2885-19/326571776_203388555551720_8229853915859029301_n.jpg?_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=4IQIxJXsOYQAb5_miWY&edm=APs17CUBAAAA&ccb=7-5&oh=00_AfAdHHpwvBWtHv-ZySHB4nTAdb6XNofONbIFOAoAHmUi2Q&oe=661D9FF1&_nc_sid=10d13b"
-                                alt="@shadcn"
+								alt="@shadcn"
 							/>
 							<Avatar.Fallback>RB</Avatar.Fallback>
 						</Avatar.Root>
@@ -98,11 +99,11 @@
 							<span class="text-sm text-muted-foreground">Back-end Developer</span>
 						</div>
 					</div>
-					<div class="border-1 flex rounded-md border gap-4 p-4 items-center">
+					<div class="border-1 flex items-center gap-4 rounded-md border p-4">
 						<Avatar.Root>
 							<Avatar.Image
 								src="https://scontent-mnl1-1.cdninstagram.com/v/t51.2885-19/413338935_922405739269869_4590117888755648847_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-mnl1-1.cdninstagram.com&_nc_cat=109&_nc_ohc=RySWv0aW6TUAb5g8ou3&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfDt6HOD2vls8dLK2ZldbaYqODh8R-Zes7Q9G6_oQ4-Xng&oe=661D9F6A&_nc_sid=8b3546"
-                                alt="@shadcn"
+								alt="@shadcn"
 							/>
 							<Avatar.Fallback>LR</Avatar.Fallback>
 						</Avatar.Root>
@@ -117,18 +118,76 @@
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Summary of Customer Priorities</Card.Title>
-				<Card.Description>text</Card.Description>
 			</Card.Header>
-			<Card.Content class="grid auto-rows-max items-start justify-center gap-3 px-24 pb-12"
-			></Card.Content>
+			<Card.Content class="grid auto-rows-max items-start justify-center gap-3 px-24 pb-12">
+				<p>
+					In summary, the main requirement of the customer is a working database system with
+					integrated form generation, account systems, and report generation.
+				</p>
+				<ul class="list-disc">
+					<li>
+						<strong>Form generation and secure database:</strong> The PAO-ERS should be able to generate
+						forms, read, and store client inputs to be transmitted to the database. On top of this, the
+						system should be able to safely and securely store the client data and should be resistant
+						to data leaks.
+					</li>
+					<li>
+						<strong>Software compatibility with the hardware:</strong> The system should be able to run
+						on the hardware or devices of the clients. This is important to avoid future expenditures
+						on equipment and to ensure fast integration of the software into the current system.
+					</li>
+					<li>
+						<strong>Account systems:</strong> The system should also have account systems that show each
+						user their respective analytics on their accounts. The account systems should also include
+						a log-in system to ensure a more secure environment for client data.
+					</li>
+					<li>
+						<strong>Fast report generation:</strong> The system should also be able to instantaneously
+						generate reports needed by the users monthly. These reports will be automatically downloaded
+						to the computer or device with a click of a button.
+					</li>
+					<li>
+						<strong>System manual and documentation:</strong> The system should come with an instruction
+						manual on how to use the system to properly notify and educate the users on the features
+						of the system. This is to ensure proper and immediate migration from the current system to
+						the digitized PAO-ERS. The documentation is important in case of future updates or improvements
+						of the system.
+					</li>
+				</ul>
+			</Card.Content>
 		</Card.Root>
 		<Card.Root>
 			<Card.Header>
 				<Card.Title>Maintenance Services</Card.Title>
-				<Card.Description>text</Card.Description>
 			</Card.Header>
 			<Card.Content class="grid auto-rows-max items-start justify-center gap-3 px-24 pb-12"
-			></Card.Content>
+				><p>
+					The developers agree to provide the following maintenance services, until the graduation
+					of the developers:
+				</p>
+				<ul class="list-disc">
+					<li>
+						<strong>Error Correction:</strong> The developers will make reasonable efforts to identify
+						and fix bugs and errors that may arise in the system's operation, ensuring its functionality
+						within intended parameters.
+					</li>
+					<li>
+						<strong>Longevity of Service:</strong> The developers will perform routine maintenance tasks
+						to ensure the system's continued operation and prevent potential issues that could affect
+						its lifespan.
+					</li>
+				</ul>
+				<Separator />
+				<p>
+					<strong>Scaling:</strong> The developers' commitment to provide free maintenance services is limited to the
+					current scope and functionality of the system. Any future requests to scale the system's
+					operations, add new features, or modify existing functionalities will be subject to the
+					developers' availability, resources, and mutual agreement with PAO Cordillera
+					Administrative Region (CAR). The developers are not obligated to fulfill such requests but
+					will engage in good faith discussions with PAO CAR regarding feasibility and potential
+					costs.
+				</p>
+			</Card.Content>
 		</Card.Root>
 		<Card.Root>
 			<Card.Header>
