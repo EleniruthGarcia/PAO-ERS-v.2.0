063a4c5fb6d3db293c06923201bca13c957ece5b
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-04-29 20:28:13 +0800

    Add server configurations

--- src/routes/(protected)/requests/add/+page.server.ts
+++ src/routes/(protected)/requests/add/+page.server.ts
@@ -56,7 +56,7 @@ export const actions: Actions = {
 		const request = await db.requests.insertOne({
 			...form.data,
 			// _id: `${branch?.region}:${branch?.district}:${new Date().getFullYear()}:${new Date().getMonth()}:${(await db.counters.findOneAndUpdate({ _id: 'requests', branch_id: branch?._id }, { $inc: { count: 1 } }, { upsert: true }))?.count}`
-			_id: `${new Date().getFullYear()}:${new Date().getMonth()}:${(await db.counters.findOneAndUpdate({ _id: 'requests', branch_id: branch?._id }, { $inc: { count: 1 } }, { upsert: true }))?.count}`
+			_id: `${new Date().getFullYear()}:${new Date().getMonth()}:${(await db.counters.findOneAndUpdate({ _id: 'requests', branch_id: branch?._id }, { $inc: { count: 1 } }, { upsert: true }))?.count ?? 0}`
 		});
 		if (!request.acknowledged) return fail(500, { form });
 
