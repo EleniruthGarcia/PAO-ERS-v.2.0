fae465cfc7b7ac193e0203782a9f6e33c08e7497
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-04-29 22:46:40 +0800

    Refactor age validation in formSchema to include error messages

--- src/lib/schema/client.ts
+++ src/lib/schema/client.ts
@@ -60,7 +60,12 @@ export const formSchema = z.object({
 	// 	invalid_type_error: 'Date of birth is required.',
 	// 	required_error: 'Date of birth is required.'
 	// }),
-	age: z.coerce.number({ invalid_type_error: 'Age is required.' }).positive().min(1, 'Age is required.'),
+	age: z.number({
+		invalid_type_error: 'Age is required.',
+		required_error: 'Age is required.'
+	}).positive({
+		message: 'Age must be a positive number.'
+	}),
 	sex: z.enum(sex),
 	address: z.string().min(1, 'Address is required.'),
 	email: z.string().email().optional(),
