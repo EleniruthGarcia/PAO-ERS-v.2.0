501cd56f40ad667c5d008adcc5df891b0c9d8f6e
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-05-01 00:50:03 +0800

    Update user ID in mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -1,5 +1,5 @@
 db.users.insertOne({
-	_id: 'admin',
+	_id: '1',
 	username: 'admin',
 	hashedPassword:
 		'$argon2id$v=19$m=19456,t=2,p=1$9pRcWSi/VmNeYOQ/JA7Mhg$GOHloucwALRVHbF7OKv1J8YMTfF0SePJU1XG20e4Nf4',
