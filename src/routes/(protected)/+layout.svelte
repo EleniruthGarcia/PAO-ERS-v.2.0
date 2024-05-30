a7b49ce9ccb0e2a08df59bcb200c6546cd8c73bf
Marilag <offcllance@gmail.com>
2024-05-30 20:35:20 +0800

    Added bug report button

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -5,8 +5,10 @@
 	import NightToggle from '$lib/components/NightToggle.svelte';
 	import Search from '$lib/components/Search.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import { LinkBreak1 } from 'svelte-radix';
 </script>
 
 <div class="flex min-h-dvh w-full bg-muted/40">
@@ -20,6 +22,14 @@
 				<Breadcrumb />
 				<Search />
 				<NightToggle />
+				<Button
+					variant="link"
+					size="icon"
+					class="border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"
+					href="https://docs.google.com/forms/d/e/1FAIpQLSeQgGCDW9ThhDKuLG9zJ1r0NKleHuZRcsYeOh19YXl7Hem5ww/viewform"
+				>
+					<LinkBreak1 class="h-4 w-4" />
+				</Button>
 				<Account />
 			</header>
 			<main class="flex-1 items-start p-4 md:px-6 md:py-0">
