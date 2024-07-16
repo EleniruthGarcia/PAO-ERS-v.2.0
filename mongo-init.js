36628d7fb2b7a0977a685acb0ef7fd47b943a204
Daniel David N. Bador <danieldavidbador@yahoo.com>
2024-07-16 14:12:00 +0800

    update mongo-init.js

--- mongo-init.js
+++ mongo-init.js
@@ -1,9 +1,32 @@
+db.branches.insertMany([
+	{
+		_id: 'car',
+		name: 'Regional Office - CAR',
+		district: 'Baguio City',
+		provice: 'Benguet',
+		region: 'Cordillera Administrative Region',
+	}, {
+		_id: 'Baguio',
+		name: 'Baguio City District Office',
+		district: 'Baguio City',
+		province: 'Benguet',
+		region: 'Cordillera Administrative Region',
+	}
+
+]);
+
 db.users.insertOne({
 	_id: '1',
 	username: 'admin',
 	name: 'Admin',
+	branch_id: 'car',
 	position: 'Administrative Officer',
+	rank: 'I',
 	hashedPassword:
 		'$argon2id$v=19$m=19456,t=2,p=1$9pRcWSi/VmNeYOQ/JA7Mhg$GOHloucwALRVHbF7OKv1J8YMTfF0SePJU1XG20e4Nf4',
-	role: 'Administrator'
+	role: 'Administrator',
+	status: [{
+		type: 'New',
+		date: Date.now(),
+	}]
 });
