47af62410b2bf23a263937762faa09eca68fabdf
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-03-24 18:14:16 +0800

    add three replica sets

--- mongo-init.js
+++ mongo-init.js
@@ -1,3 +1,11 @@
+db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE)
+
+db.createUser({
+	user: process.env.DB_USERNAME,
+	pwd: process.env.DB_PASSWORD,
+	roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }]
+}, { w: 'majority', wtimeout: 5000 });
+
 db.branches.insertMany([
 	{
 		_id: 'CAR',
