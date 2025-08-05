52141ffeea75040f6af8355fa9a006f4646f245d
IStealPotatoesBTW <elijah3006@gmail.com>
2025-08-05 13:10:51 +0800

    days update

--- src/lib/schema/report.ts
+++ src/lib/schema/report.ts
@@ -54,11 +54,8 @@ export const formSchema = z.object({
 		required_error: 'Month is required.',
 		invalid_type_error: 'Invalid month!'
 	}),
-	day: z.number().min(1, 'Day is required.'),
-	// month: z.enum(months, {
-	// 	required_error: 'Month is required!'
-	// }),
-	year: z.number().min(1, 'Year is required.'),
+	day: z.coerce.number().min(1, 'Day is required.'),   // <-- use coerce here
+    year: z.coerce.number().min(1, 'Year is required.'), // <-- and here if needed
 	notedBy: z.string().min(1, 'Please indicate who noted the report.'),
 	reports: z.array(z.enum(reports))
 });
